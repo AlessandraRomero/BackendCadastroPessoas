edcf8e9362e2d0637bced476d99f20307fe4c6d7
Alessandra Romero <alessandraromeiro098@gmail.com>
2023-06-26 11:01:03 -0400

    Adicionando rotas

--- src/rotas/index.ts
+++ src/rotas/index.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
+import { bairroRouter } from './bairro';
+import { enderecoRouter } from './endereco';
+import { municipioRouter } from './municipio';
 import { pessoasRouter } from './pessoa';
+import { ufRouter } from './uf';
 
 const rotas = Router();
 
 rotas.use(pessoasRouter);
+rotas.use(enderecoRouter);
+rotas.use(bairroRouter);
+rotas.use(municipioRouter);
+rotas.use(ufRouter);
 
 export { rotas };
