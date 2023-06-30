834b46d04d8f5a663ec78cfb5d24dd4fcce1038e
Alessandra Romero <alessandraromeiro098@gmail.com>
2023-06-30 12:30:31 -0400

    ajustes criarBairro

--- src/controllers/bairroController.ts
+++ src/controllers/bairroController.ts
@@ -1,51 +1,66 @@
 import { Request, Response } from 'express';
 import { Municipio } from '../entidades/Municipio';
 import { atualizarBairro } from '../services/bairro/atualizarBairro';
+import { buscarBairroPor } from '../services/bairro/buscarBairroPor';
 import { buscarBairros } from '../services/bairro/buscarBairros';
 import { excluirBairro } from '../services/bairro/excluirBairro';
 import { inserirBairro } from '../services/bairro/inserirBairro';
-import { buscarPessoaPorId } from '../services/pessoa/buscarPessoaPorId';
+import { buscarMunicipioPor } from '../services/municipio/buscarMunicipioPor';
 
 interface IRequest {
   codigoBairro: number;
   nome: string;
   status: number;
-  codigoMunicipio: Municipio;
+  codigoMunicipio: number;
 }
 
 async function listarBairros(req: Request, res: Response) {
-  const bairos = await buscarBairros();
+  const filtros: IFilter = req.query;
+  const bairos = await buscarBairroPor(filtros);
 
   if (bairos) return res.status(200).send(bairos);
 
   return res.status(404).send({
-    mensagem: 'Não foi possível encontrar todos os bairros',
+    mensagem: 'Não foi possível consultar município no banco de dados.',
     status: 404,
   });
 }
-
-async function listaBairroPorId(req: Request, res: Response) {
-  const codigoBairro = Number(req.params.id);
-
-  const bairro = await buscarPessoaPorId(codigoBairro);
-
-  if (bairro) return res.send(bairro);
-
-  return res.status(404).send({
-    Error: 'Não foi possíve encontrar bairro por id',
-    status: 404,
-  });
+interface IFilter {
+  codigoBairro?: number;
+  nome?: string;
+  status?: number;
+  codigoMunicipio?: Municipio;
 }
 
 async function criarBairro(req: Request, res: Response) {
   const bairroDados: IRequest = req.body;
-  const resultado = await inserirBairro(bairroDados);
+  const municipioExiste = await buscarMunicipioPor({
+    codigoMunicipio: bairroDados.codigoMunicipio,
+  });
+
+  if (!municipioExiste) {
+    return res.status(404).send({
+      mensagem: 'Não foi possível localizar bairro',
+      status: 404,
+    });
+  }
+  const bairroNovo = {
+    nome: bairroDados.nome,
+    status: bairroDados.status,
+    codigoMunicipio: municipioExiste[0],
+  };
+
+  const resultado = await inserirBairro(bairroNovo);
 
   if (resultado) {
     const bairros = await buscarBairros();
     return res.status(200).send(bairros);
   }
-  return res.status(404).send({});
+
+  return res.status(404).send({
+    mensagem: 'Não foi possível incluir bairro no banco de dados.',
+    status: 404,
+  });
 }
 
 async function atualizaBairro(req: Request, res: Response) {
@@ -71,10 +86,4 @@ async function excluiBairro(req: Request, res: Response) {
   });
 }
 
-export {
-  atualizaBairro,
-  criarBairro,
-  excluiBairro,
-  listaBairroPorId,
-  listarBairros,
-};
+export { atualizaBairro, criarBairro, excluiBairro, listarBairros };
