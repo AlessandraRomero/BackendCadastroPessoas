f22dd057939d6acfe0b3a1c01ddd816ac642684f
Alessandra Romero <alessandraromeiro098@gmail.com>
2023-06-30 12:33:19 -0400

    ajustes buscarBairros

--- src/services/bairro/buscarBairros.ts
+++ src/services/bairro/buscarBairros.ts
@@ -3,7 +3,26 @@ import { Bairro } from '../../entidades/Bairro';
 
 async function buscarBairros() {
   const bairroRepository = AppDataSource.getRepository(Bairro);
-  const bairro = await bairroRepository.find();
-  return bairro;
+  const bairros = await bairroRepository
+    .createQueryBuilder('bairro')
+    .innerJoin('bairro.codigoMunicipio', 'municipio')
+    .select([
+      'bairro.codigoBairro',
+      'bairro.nome',
+      'bairro.status',
+      'municipio.codigoMunicipio',
+    ])
+    .getMany();
+
+  const bairrosFiltrados = bairros.map(bairro => {
+    return {
+      codigoBairro: bairro.codigoBairro,
+      nome: bairro.nome,
+      status: bairro.status,
+      codigoMunicipio: bairro.codigoMunicipio.codigoMunicipio,
+    };
+  });
+
+  return bairrosFiltrados;
 }
 export { buscarBairros };
