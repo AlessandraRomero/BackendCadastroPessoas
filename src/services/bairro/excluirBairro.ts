c3f6683004ffe468a0ce88203236fd6326e33e4b
Alessandra Romero <alessandraromeiro098@gmail.com>
2023-07-02 10:43:11 -0400

    adicionando função buscarBairros()

--- src/services/bairro/excluirBairro.ts
+++ src/services/bairro/excluirBairro.ts
@@ -1,10 +1,11 @@
 import { AppDataSource } from '../../AppDataSource';
 import { Bairro } from '../../entidades/Bairro';
+import { buscarBairros } from './buscarBairros';
 
 async function excluirBairro(codigoBairro: number) {
   const bairroRepository = AppDataSource.getRepository(Bairro);
   const bairro = await bairroRepository.delete({ codigoBairro });
-  const bairros = await bairroRepository.find();
+  const bairros = await buscarBairros();
 
   if (!bairro.affected) {
     return null;
