34c60b839c6688444c27913ba94ac14e42e338a1
Alessandra Romero <alessandraromeiro098@gmail.com>
2023-07-04 11:14:57 -0400

    criando service para buscar pessoas

--- src/services/pessoa/buscarPessoas.ts
+++ src/services/pessoa/buscarPessoas.ts
@@ -4,10 +4,29 @@ import { Pessoa } from '../../entidades/Pessoa';
 async function buscarPessoas() {
   const pessoaRepository = AppDataSource.getRepository(Pessoa);
   const pessoas = await pessoaRepository.find({
-    relations: {
-      enderecos: true,
+    select: [
+      'codigoPessoa',
+      'nome',
+      'sobrenome',
+      'idade',
+      'login',
+      'senha',
+      'status',
+    ],
+    relations: ['enderecos'],
+    join: {
+      alias: 'pessoa',
+      leftJoinAndSelect: {
+        enderecos: 'pessoa.enderecos',
+      },
     },
+    loadRelationIds: true,
   });
+
+  pessoas.forEach(pessoa => {
+    pessoa.enderecos = [];
+  });
+
   return pessoas;
 }
 export { buscarPessoas };
