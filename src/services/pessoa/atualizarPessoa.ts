689875a587b84ee4ad248974e122da14489dcaa2
Alessandra Romero <alessandraromeiro098@gmail.com>
2023-07-04 11:14:10 -0400

    criando service para atualizar Pessoa e seus respectivos endereços

--- src/services/pessoa/atualizarPessoa.ts
+++ src/services/pessoa/atualizarPessoa.ts
@@ -1,7 +1,10 @@
 import { AppDataSource } from '../../AppDataSource';
+import { Bairro } from '../../entidades/Bairro';
+import { Endereco } from '../../entidades/Endereco';
 import { Pessoa } from '../../entidades/Pessoa';
+import { buscarPessoas } from './buscarPessoas';
 
-interface IRequest {
+interface IDadosPessoa {
   codigoPessoa: number;
   nome: string;
   sobrenome: string;
@@ -9,14 +12,26 @@ interface IRequest {
   login: string;
   senha: string;
   status: number;
+  enderecos: {
+    codigoEndereco: number;
+    nomeRua: string;
+    numero: string;
+    complemento: string;
+    cep: string;
+    codigoPessoa: Pessoa;
+    codigoBairro: Bairro;
+  }[];
 }
-async function atualizarPessoa(pessoa: IRequest) {
+async function atualizarPessoa(pessoa: IDadosPessoa) {
   const pessoaRepository = AppDataSource.getRepository(Pessoa);
-  const pessoaExiste = await pessoaRepository.findOneBy({
-    codigoPessoa: pessoa.codigoPessoa,
+  const enderecoRepository = AppDataSource.getRepository(Endereco);
+
+  const pessoaExiste = await pessoaRepository.findOne({
+    where: { codigoPessoa: pessoa.codigoPessoa },
+    relations: ['enderecos'],
   });
 
-  if (pessoaExiste === null) {
+  if (!pessoaExiste) {
     return null;
   }
   pessoaExiste.nome = pessoa.nome;
@@ -25,6 +40,40 @@ async function atualizarPessoa(pessoa: IRequest) {
   pessoaExiste.login = pessoa.login;
   pessoaExiste.senha = pessoa.senha;
   pessoaExiste.status = pessoa.status;
-  return pessoaRepository.save(pessoaExiste);
+
+  await pessoaRepository.save(pessoaExiste);
+
+  for (const enderecoExistente of pessoaExiste.enderecos) {
+    const enderecoCorrespondente = pessoa.enderecos.find(
+      endereco => endereco.codigoEndereco === enderecoExistente.codigoEndereco,
+    );
+
+    if (enderecoCorrespondente) {
+      // Atualiza o endereço existente
+      enderecoExistente.nomeRua = enderecoCorrespondente.nomeRua;
+      enderecoExistente.numero = enderecoCorrespondente.numero;
+      enderecoExistente.complemento = enderecoCorrespondente.complemento;
+      enderecoExistente.cep = enderecoCorrespondente.cep;
+      enderecoExistente.codigoPessoa = enderecoCorrespondente.codigoPessoa;
+      enderecoExistente.codigoBairro = enderecoCorrespondente.codigoBairro;
+
+      await enderecoRepository.save(enderecoExistente);
+    } else {
+      await enderecoRepository.remove(enderecoExistente);
+    }
+  }
+
+  pessoa.enderecos.forEach(async endereco => {
+    if (!endereco.codigoEndereco) {
+      const novoEndereco = enderecoRepository.create(endereco);
+      novoEndereco.codigoPessoa = pessoaExiste;
+      pessoaExiste.enderecos.push(novoEndereco);
+
+      await enderecoRepository.save(novoEndereco);
+    }
+  });
+  const pessoas = await buscarPessoas();
+
+  return pessoas;
 }
 export { atualizarPessoa };
