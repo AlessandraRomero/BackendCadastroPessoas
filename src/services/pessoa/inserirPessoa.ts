9369851b55a78133e4be4df03d0908d1e0da0756
Alessandra Romero <alessandraromeiro098@gmail.com>
2023-07-04 11:16:15 -0400

    criando service para  inserir uma pessoa e seus enderecos

--- src/services/pessoa/inserirPessoa.ts
+++ src/services/pessoa/inserirPessoa.ts
@@ -1,23 +1,42 @@
 import { AppDataSource } from '../../AppDataSource';
+import { Endereco } from '../../entidades/Endereco';
 import { Pessoa } from '../../entidades/Pessoa';
 
-interface IRequest {
+export interface IDadosPessoa {
   nome: string;
   sobrenome: string;
   idade: number;
   login: string;
   senha: string;
   status: number;
+  enderecos: Endereco[];
 }
 
-async function inserirPessoa(pessoaDados: IRequest) {
+async function inserirPessoa(pessoaDados: IDadosPessoa) {
   const pessoaRepository = AppDataSource.getRepository(Pessoa);
-  const pessoa: IRequest = pessoaDados;
-  const pessoaExiste = await pessoaRepository.findOneBy(pessoa);
+  const enderecoRepository = AppDataSource.getRepository(Endereco);
+  const pessoaExistente = await pessoaRepository.findOne({
+    where: { login: pessoaDados.login },
+  });
 
-  if (pessoaExiste) {
+  if (pessoaExistente) {
     return null;
   }
-  return pessoaRepository.save(pessoa);
+  if (pessoaDados.enderecos.length === 0) {
+    return null;
+  }
+  const novaPessoa = pessoaRepository.create(pessoaDados);
+
+  const novosEnderecos = pessoaDados.enderecos.map(endereco =>
+    enderecoRepository.create(endereco),
+  );
+  novaPessoa.enderecos = novosEnderecos;
+  await pessoaRepository.save(novaPessoa);
+
+  const pessoasComEnderecos = await pessoaRepository.find({
+    relations: ['enderecos'],
+  });
+
+  return pessoasComEnderecos;
 }
 export { inserirPessoa };
