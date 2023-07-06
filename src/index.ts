d5584d6741439f1717d224bac62d06c7dceb6cad
Alessandra Romero <alessandraromeiro098@gmail.com>
2023-07-05 21:13:51 -0400

    adicionando tratamento de erro celebrate

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { CelebrateError } from 'celebrate';
 import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
+import { ValidationError } from 'joi';
 import { AppDataSource } from './AppDataSource';
 import { rotas } from './rotas';
 
@@ -13,17 +14,14 @@ AppDataSource.initialize().then(() => {
   app.use(rotas);
 
   app.use(
-    (
-      error: Error,
-      request: Request,
-      response: Response,
-      next: NextFunction,
-    ) => {
+    (error: any, request: Request, response: Response, next: NextFunction) => {
       if (error instanceof CelebrateError) {
-        const errorBody = error.details.get('body');
+        const errorBody = Array.from(error.details.values()).map(
+          (error: ValidationError) => error.message,
+        );
         return response.status(400).json({
           status: '400',
-          mensagem: errorBody?.message,
+          mensagem: errorBody.join(', '), // Concatenando as mensagens de erro em uma única string separada por vírgula
         });
       }
       return response.status(500).json({
