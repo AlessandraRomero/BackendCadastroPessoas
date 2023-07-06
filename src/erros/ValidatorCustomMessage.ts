4527e80fa1378d5fe1c57290fe026319d81a9a7a
Alessandra Romero <alessandraromeiro098@gmail.com>
2023-07-05 21:20:23 -0400

    criando mensagens de validação customizadas

--- src/erros/ValidatorCustomMessage.ts
+++ src/erros/ValidatorCustomMessage.ts
@@ -1,7 +1,15 @@
+const max256Caracter = 256;
+
 const mensagemCustomizada = (field: string) => {
   return {
-    'string.empty': `o campo ${field} não pode ser vazio`,
-    'any.required': `o campo ${field} é obrigatório`,
+    'string.max': `O campo ${field}  não pode exceder 256 caracteres.`,
+    'string.base': `O campo ${field} deve ser uma string.`,
+    'string.empty': `O campo ${field} não pode ser vazio.`,
+    'any.required': `O campo ${field} é obrigatório.`,
+    'any.invalid': `O campo ${field} deve ser um número.`,
+    'number.base': `O campo ${field} deve ser um número.`,
+    'number.max': `O campo ${field} não pode exceder o máximo de 3 dígitos.`,
   };
 };
+
 export { mensagemCustomizada };
