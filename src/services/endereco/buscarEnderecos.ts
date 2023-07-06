2d3e860f2a30a9c3eb8503d5774d289a58c1210b
Alessandra Romero <alessandraromeiro098@gmail.com>
2023-07-05 21:18:41 -0400

    removendo arquivos desnecessários

--- src/services/endereco/buscarEnderecos.ts
+++
@@ -1,9 +0,0 @@
-import { AppDataSource } from '../../AppDataSource';
-import { Endereco } from '../../entidades/Endereco';
-
-async function buscarEnderecos() {
-  const enderecoRepository = AppDataSource.getRepository(Endereco);
-  const endereco = await enderecoRepository.find();
-  return endereco;
-}
-export { buscarEnderecos };
