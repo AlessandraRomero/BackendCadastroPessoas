a09a23c5ff5d0e8f75e29d057974048462bb0021
Alessandra Romero <alessandraromeiro098@gmail.com>
2023-07-27 19:18:08 -0400

    verificando uf por sigla e nome

--- src/services/uf/inserirUf.ts
+++ src/services/uf/inserirUf.ts
@@ -1,5 +1,6 @@
 import { AppDataSource } from '../../AppDataSource';
 import { Uf } from '../../entidades/Uf';
+import { verificarUfExistente } from './verificarUfExistente';
 
 interface IRequest {
   codigoUF: number;
@@ -10,12 +11,12 @@ interface IRequest {
 
 async function inserirUf(ufDados: IRequest) {
   const ufRepository = AppDataSource.getRepository(Uf);
-  const uf: IRequest = ufDados;
-  const ufExiste = await ufRepository.findOneBy(uf);
+  const { sigla, nome }: IRequest = ufDados;
+  const ufExiste = await verificarUfExistente(sigla, nome);
 
   if (ufExiste) {
     return null;
   }
-  return ufRepository.save(uf);
+  return ufRepository.save(ufDados);
 }
 export { inserirUf };
